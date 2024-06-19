6d518ad485638612a83133a23e96bfed6be992bd
pedrohmv <pedrohmvieira04@gmail.com>
2024-06-19 16:25:10 -0300

    Code Refactoring done

--- src/data.py
+++ src/data.py
@@ -1,237 +1,248 @@
 import streamlit as st
+from os import path
 from streamlit_echarts import st_echarts
-from streamlit_option_menu import option_menu
-import pandas as pd
+from pandas import read_csv
 
-def stage_means(column):
-    # Função para calcular a média por estágio
-    df_cirrose = pd.read_csv('data/liver_cirrhosis_v1.csv')
-    return df_cirrose.groupby('Estágio')[column].mean().tolist()
+from src.config import Config
 
-def app():
-    # Carregamento do arquivo CSV (substitua pelo seu caminho e nome de arquivo)
-    df_cirrose = pd.read_csv('data/liver_cirrhosis_v1.csv')
+class Data:
+    """Data section class"""
 
-    # Menu de seleção
-    option = st.selectbox(
-        "Selecione o Gráfico",
-        ("Bilirrubina", 
-         "Expectativa de Vida",
-         "Mortalidade por Estágio",
-         "Mortalidade por Sexo")
-    )
+    def __init__(self):
+        self.config = Config()
+        self.data_path = path.join(self.config.vars.data_dir, self.config.vars.data_csv) # CSV data file path
+        self.df_cirrose = read_csv(self.data_path) # Load the cirrhosis dataset
 
-    if option == "Bilirrubina":
-        # Configuração do gráfico ECharts para média de bilirrubina por estágio
-        albValues = stage_means('Bilirrubina(mg/dl)')
-        stages = ["Estágio 1", "Estágio 2", "Estágio 3"]
+    def stage_means(self, column: str) -> object:
+        """Calculate the mean of a column by stage
+           column: str - column name
+           return: DataFrame
+        """
+        return self.df_cirrose.groupby('Estágio')[column].mean().tolist()
 
-        option_bilirrubina = {
-            "xAxis": {
-                "type": "category",
-                "data": stages,
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo X
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo X
-                }
-            },
-            "yAxis": {
-                "type": "value",
-                "min": 0,
-                "max": 5,
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo Y
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo Y
-                }
-            },
-            "series": [
-                {
-                    "name": "Média de Bilirrubina (mg/dl)",
-                    "data": albValues,
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#d94c21"  # Green color for "Média de Bilirrubina (mg/dl)"
+    def data(self) -> None:
+        """Data visualization section geneator function
+           return: None
+        """
+
+        # Selection Menu
+        option = st.selectbox(
+            "Selecione o Gráfico",
+            ("Bilirrubina", 
+            "Expectativa de Vida",
+            "Mortalidade por Estágio",
+            "Mortalidade por Sexo")
+        )
+
+        if option == "Bilirrubina":
+            # Calculate the average bilirubin for each stage
+            albValues = self.stage_means('Bilirrubina(mg/dl)')
+            stages = ["Estágio 1", "Estágio 2", "Estágio 3"]
+
+            # chart configuration
+            option_bilirrubina = {
+                "xAxis": {
+                    "type": "category",
+                    "data": stages,
+                    "axisLabel": {
+                        "color": "#ffffff"  # X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
                     }
                 },
-            ],
-            "legend": {"data": ["Média de Bilirrubina (mg/dl)"],
-                       "textStyle": {
-                            "color": 'white'
-                        }},
-            "backgroundColor": "rgb(14, 17, 23)"
-        }
-
-        # Exibindo o gráfico ECharts de média de bilirrubina por estágio
-        st.title("Qtd. Média de Bilirrubina em Cada Estágio da Cirrose")
-        st_echarts(options=option_bilirrubina, height="600px")
-
-    elif option == "Expectativa de Vida":
-        # Calcular tempo médio para morte em cada estágio
-        deathTime1 = df_cirrose.loc[(df_cirrose['Situação'] == 'Morte') & (df_cirrose['Estágio'] == 1), 'Numero_dias'].mean()
-        deathTime2 = df_cirrose.loc[(df_cirrose['Situação'] == 'Morte') & (df_cirrose['Estágio'] == 2), 'Numero_dias'].mean()
-        deathTime3 = df_cirrose.loc[(df_cirrose['Situação'] == 'Morte') & (df_cirrose['Estágio'] == 3), 'Numero_dias'].mean()
-
-        # Configuração do gráfico ECharts para tempo médio de morte por estágio
-        option_tempo_morte = {
-            "xAxis": {
-                "type": "category",
-                "data": ["Estágio 1", "Estágio 2", "Estágio 3"],
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo X
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo X
-                }
-            },
-            "yAxis": {
-                "type": "value",
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo Y
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo Y
-                }
-            },
-            "series": [
-                {
-                    "name": "Tempo médio de morte (dias)",
-                    "data": [deathTime1, deathTime2, deathTime3],
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#b02238"  # Red color for tempo médio de morte
+                "yAxis": {
+                    "type": "value",
+                    "min": 0,
+                    "max": 5,
+                    "axisLabel": {
+                        "color": "#ffffff"  # Y axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # Y axis name color
                     }
                 },
-            ],
-
-            "legend": {"data": ["Tempo médio de morte (dias)"],
-                       "textStyle": {
-                            "color": 'white'
-                            },
-                        },
-            "backgroundColor": "rgb(14, 17, 23)"
-        }
-
-        # Exibindo o gráfico ECharts de tempo médio de morte por estágio
-        st.title("Tempo Médio para Morte em Cada Estágio da Cirrose")
-        st_echarts(options=option_tempo_morte, height="600px")
-
-    elif option == "Mortalidade por Estágio":
-        # Configuração do gráfico ECharts para contagem por Estágio e Situação
-        df_filtrado = df_cirrose[df_cirrose['Situação'] != 'Transplante']
-        contagem_por_estagio = df_filtrado.groupby(['Estágio', 'Situação']).size().unstack()
-
-        option_estagio = {
-            "xAxis": {
-                "type": "category",
-                "data": contagem_por_estagio.index.tolist(),
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo X
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo X
-                }
-            },
-            "backgroundColor": "rgb(14, 17, 23)",
-            "yAxis": {
-                "type": "value",
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo Y
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo Y
-                }
-            },
-            "series": [
-                {
-                    "name": "Morte",
-                    "data": contagem_por_estagio['Morte'].tolist(),
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#b02238"  # Red color for "Morte"
-                    }
-                },
-                {
-                    "name": "Sobreviveu",
-                    "data": contagem_por_estagio['Sobreviveu'].tolist(),
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#4682B4"  # Blue color for "Sobreviveu"
-                    }
-                },
-            ],
-            "legend": {"data": ["Morte", "Sobreviveu"],
-                       "textStyle": {
-                            "color": 'white'
-                            },
+                "series": [
+                    {
+                        "name": "Média de Bilirrubina (mg/dl)",
+                        "data": albValues,
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#d94c21"  # Green color for "Média de Bilirrubina (mg/dl)"
                         }
-        }
+                    },
+                ],
+                "legend": {"data": ["Média de Bilirrubina (mg/dl)"],
+                        "textStyle": {
+                                "color": 'white'
+                            }},
+                "backgroundColor": "rgb(14, 17, 23)"
+            }
 
-        # Exibindo o gráfico ECharts de contagem por Estágio e Situação
-        st.title("Distribuição de Sobreviventes e Óbitos por Estágio da Cirrose")
-        st_echarts(options=option_estagio, height="600px")
+            # Plot chart
+            st.title("Qtd. Média de Bilirrubina em Cada Estágio da Cirrose")
+            st_echarts(options=option_bilirrubina, height="600px")
 
-    elif option == "Mortalidade por Sexo":
-        # Considerando o valor de "Transplante" como "Sobreviveu"
-        df_cirrose['Situação'] = df_cirrose['Situação'].replace('Transplante', 'Sobreviveu')
+        if option == "Expectativa de Vida":
+            # Calculate the average death time for each stage
+            deathTime1 = self.df_cirrose.loc[(self.df_cirrose['Situação'] == 'Morte') & (self.df_cirrose['Estágio'] == 1), 'Numero_dias'].mean()
+            deathTime2 = self.df_cirrose.loc[(self.df_cirrose['Situação'] == 'Morte') & (self.df_cirrose['Estágio'] == 2), 'Numero_dias'].mean()
+            deathTime3 = self.df_cirrose.loc[(self.df_cirrose['Situação'] == 'Morte') & (self.df_cirrose['Estágio'] == 3), 'Numero_dias'].mean()
 
-        # Filtrando e agrupando o DataFrame para contagem por Sexo e Situação
-        contagem_por_sexo = df_cirrose.groupby(['Sexo', 'Situação']).size().unstack()
-
-        # Configuração do gráfico ECharts para contagem por Sexo e Situação
-        option_sexo = {
-            "xAxis": {
-                "type": "category",
-                "data": contagem_por_sexo.index.tolist(),
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo X
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo X
-                }
-            },
-            "yAxis": {
-                "type": "value",
-                "axisLabel": {
-                    "color": "#ffffff"  # Cor do texto do eixo Y
-                },
-                "nameTextStyle": {
-                    "color": "#ffffff"  # Cor do nome do eixo Y
-                }
-            },
-            "series": [
-                {
-                    "name": "Mortos",
-                    "data": contagem_por_sexo['Morte'].tolist(),
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#b02238"  # Red color for "Mortos"
+            # Chart configuration
+            option_tempo_morte = {
+                "xAxis": {
+                    "type": "category",
+                    "data": ["Estágio 1", "Estágio 2", "Estágio 3"],
+                    "axisLabel": {
+                        "color": "#ffffff"  # X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
                     }
                 },
-                {
-                    "name": "Sobreviventes/Transplantados",
-                    "data": contagem_por_sexo['Sobreviveu'].tolist(),
-                    "type": "bar",
-                    "itemStyle": {
-                        "color": "#4682B4"  # Blue color for "Sobreviventes/Transplantados"
+                "yAxis": {
+                    "type": "value",
+                    "axisLabel": {
+                        "color": "#ffffff"  #  X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
                     }
                 },
-            ],
-            "legend": {"data": ["Mortos", "Sobreviventes/Transplantados"],
-                       "textStyle": {
-                            "color": 'white'
-                            },
-                            },
-            "backgroundColor": "rgb(14, 17, 23)",
-        }
+                "series": [
+                    {
+                        "name": "Tempo médio de morte (dias)",
+                        "data": [deathTime1, deathTime2, deathTime3],
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#b02238"  # Red color for "tempo médio de morte"
+                        }
+                    },
+                ],
 
-        # Exibindo o gráfico ECharts de contagem por Sexo e Situação
-        st.title("Quantidade de Mortos e Sobreviventes/Transplantados por Sexo")
-        st_echarts(options=option_sexo, height="600px")
+                "legend": {"data": ["Tempo médio de morte (dias)"],
+                        "textStyle": {
+                                "color": 'white'
+                                },
+                            },
+                "backgroundColor": "rgb(14, 17, 23)"
+            }
 
-# Executando o aplicativo
-if __name__ == "__main__":
-    app()
+            # Plot chart
+            st.title("Tempo Médio para Morte em Cada Estágio da Cirrose")
+            st_echarts(options=option_tempo_morte, height="600px")
+
+        if option == "Mortalidade por Estágio":
+            # Deaths for stage
+            df_filtrado = self.df_cirrose[self.df_cirrose['Situação'] != 'Transplante']
+            contagem_por_estagio = df_filtrado.groupby(['Estágio', 'Situação']).size().unstack()
+
+            # Chart configuration
+            option_estagio = {
+                "xAxis": {
+                    "type": "category",
+                    "data": contagem_por_estagio.index.tolist(),
+                    "axisLabel": {
+                        "color": "#ffffff"  # X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
+                    }
+                },
+                "backgroundColor": "rgb(14, 17, 23)",
+                "yAxis": {
+                    "type": "value",
+                    "axisLabel": {
+                        "color": "#ffffff"  # X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
+                    }
+                },
+                "series": [
+                    {
+                        "name": "Morte",
+                        "data": contagem_por_estagio['Morte'].tolist(),
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#b02238"  # Red color for "Morte"
+                        }
+                    },
+                    {
+                        "name": "Sobreviveu",
+                        "data": contagem_por_estagio['Sobreviveu'].tolist(),
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#4682B4"  # Blue color for "Sobreviveu"
+                        }
+                    },
+                ],
+                "legend": {"data": ["Morte", "Sobreviveu"],
+                        "textStyle": {
+                                "color": 'white'
+                                },
+                            }
+            }
+
+            # Plot chart
+            st.title("Distribuição de Sobreviventes e Óbitos por Estágio da Cirrose")
+            st_echarts(options=option_estagio, height="600px")
+
+        if option == "Mortalidade por Sexo":
+            # Deaths for Gender considering 'Transplante' as 'Sobreviveu'
+            self.df_cirrose['Situação'] = self.df_cirrose['Situação'].replace('Transplante', 'Sobreviveu')
+
+            # Grouping the dataset
+            contagem_por_sexo = self.df_cirrose.groupby(['Sexo', 'Situação']).size().unstack()
+
+            # Chart configuration
+            option_sexo = {
+                "xAxis": {
+                    "type": "category",
+                    "data": contagem_por_sexo.index.tolist(),
+                    "axisLabel": {
+                        "color": "#ffffff"  # X axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # X axis name color
+                    }
+                },
+                "yAxis": {
+                    "type": "value",
+                    "axisLabel": {
+                        "color": "#ffffff"  # Y axis text color
+                    },
+                    "nameTextStyle": {
+                        "color": "#ffffff"  # Y axis name color
+                    }
+                },
+                "series": [
+                    {
+                        "name": "Mortos",
+                        "data": contagem_por_sexo['Morte'].tolist(),
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#b02238"  # Red color for "Mortos"
+                        }
+                    },
+                    {
+                        "name": "Sobreviventes/Transplantados",
+                        "data": contagem_por_sexo['Sobreviveu'].tolist(),
+                        "type": "bar",
+                        "itemStyle": {
+                            "color": "#4682B4"  # Blue color for "Sobreviventes/Transplantados"
+                        }
+                    },
+                ],
+                "legend": {"data": ["Mortos", "Sobreviventes/Transplantados"],
+                        "textStyle": {
+                                "color": 'white'
+                                },
+                                },
+                "backgroundColor": "rgb(14, 17, 23)",
+            }
+
+            # Plot chart
+            st.title("Quantidade de Mortos e Sobreviventes/Transplantados por Sexo")
+            st_echarts(options=option_sexo, height="600px")
--- src/home.py
+++ src/home.py
@@ -1,80 +1,57 @@
 import streamlit as st
+from os import path
+from src.config import Config
 
-def app():
-    st.markdown(
-    """
-    <style>
-        div[data-testid="column"]:nth-of-type(2)
-        {
-            display: flex;
-            justify-content: center;
-            text-align: left;
-        }
-        div[data-testid="column"]:nth-of-type(1)
-        {
-            display: flex;
-            justify-content: center;
-        }
-        div[data-testid="column"]:nth-of-type(3)
-        {
-            display: flex;
-            justify-content: center;
-        }
-        div[data-testid="column"]:nth-of-type(4)
-        {
-            display: flex;
-            justify-content: center;
-        }
-        .container {
-        display: flex;
-        align-items: center;
-        justify-content: center
-        }
+class Home:
+    """Home section class"""
 
-        img {
-        max-width: 100%;
-        max-height:100%;
-        }
+    def __init__(self):
+        self.config = Config()
+        self.logo_gif_path = self.config.vars.logo_gif # Logo gif path
+        self.linhas_gif_path = self.config.vars.linhas_gif # Lines gif path
 
-        .text {
-        font-size: 20px;
-        padding-left: 20px;
-        }
-    </style>
-    """,unsafe_allow_html=True
-)
-    # Dividir em duas células, a logo e o titulo
-    st.markdown("""
-                <div class='container'>
-                    <img src='https://raw.githubusercontent.com/ricktherunner/CirrosifierApp-TRAINEES1/main/img/spinning_logo.gif' style='width:200px;height:200px;' alt='GitHub'>
-                        <div class='text'>
-                            <h1>Cirrosifier</h1>
-                            <h2>Classificando a Cirrose</h2>
-                            <h3>Um projeto por Trainees 1<br>TAIL - 2024.1</h3>
-                        </div>
-                </div>
-                """
-                , unsafe_allow_html=True)
+    def home(self):
+        """Home section generator function
+           return: None
+        """
 
-    st.markdown("---")
+        # Set the home section style
+        st.markdown(self.config.home_style, unsafe_allow_html=True)
+        
+        # Home container
+        st.markdown(f"""
+                    <div class='container'>
+                        <img src={self.logo_gif_path} style='width:200px;height:200px;' alt='GitHub'>
+                            <div class='text'>
+                                <h1>{self.config.vars.project_name}</h1>
+                                <h2>Classificando a Cirrose</h2>
+                                <h3>Um projeto por Trainees 1<br>TAIL - 2024.1</h3>
+                            </div>
+                    </div>
+                    """
+                    , unsafe_allow_html=True)
 
-    st.header("Bem Vindo!")
-    st.markdown("""
-                Você já tinha pensado em juntar **Cirrose**, **Inteligência Artificial** e **Análise de Dados**? Ainda não? 
-                Agora, para te ajudar a visualizar melhor esse cenário temos o *<strong>Cirrosfier</strong>*, um projeto que integra essas três áreas. Focado na Cirrose Biliar Primária (CBP), o Cirrosfier contém análise de dados e aplicação de modelos de classificação a fim de “prever” a possível situação (sobrevivente, morte ou transplante) e o possível estágio (1, 2, ou 3) do paciente.
-                
-                Neste site, ao informar alguns dados de taxas e a presença ou não de fatores agravantes, você poderá simular a situação e o estágio de um paciente. O modelo que fará as classificações durante a simulação será o <u>Light Gradient-Boosting Machine (LGBM)</u>, pois ao longo do projeto desenvolvido foi o que apresentou a melhor precisão dos resultados. Vale ressaltar que o dataset usado para o treino e o teste do modelo foi o *Liver Cirrhosis Stage Classification* disponível no Kaggle. 
+        st.markdown("---")
 
-                """, unsafe_allow_html=True)
-    
-    st.markdown("---")
+        # Home content
+        st.header("Bem Vindo!")
+        st.markdown(f"""
+                    Você já tinha pensado em juntar **Cirrose**, **Inteligência Artificial** e **Análise de Dados**? Ainda não? 
+                    Agora, para te ajudar a visualizar melhor esse cenário temos o *<strong>{self.config.vars.project_name}</strong>*, um projeto que integra essas três áreas. Focado na Cirrose Biliar Primária (CBP), o Cirrosfier contém análise de dados e aplicação de modelos de classificação a fim de “prever” a possível situação (sobrevivente, morte ou transplante) e o possível estágio (1, 2, ou 3) do paciente.
+                    
+                    Neste site, ao informar alguns dados de taxas e a presença ou não de fatores agravantes, você poderá simular a situação e o estágio de um paciente. O modelo que fará as classificações durante a simulação será o <u>Light Gradient-Boosting Machine (LGBM)</u>, pois ao longo do projeto desenvolvido foi o que apresentou a melhor precisão dos resultados. Vale ressaltar que o dataset usado para o treino e o teste do modelo foi o *Liver Cirrhosis Stage Classification* disponível no Kaggle. 
 
-    st.header("Navegação")
-    st.markdown("Clique nos botões da barra lateral para navegar pelo site:")
-    st.markdown("""
-                * <strong>Home:</strong> Onde você se encontra!
-                * <strong>Projeto:</strong> Aqui você verá mais informações sobre o nosso projeto
-                * <strong>Dados:</strong> Aqui você verá alguns gráficos gerados durante a análise do dataset e da aplicação dos modelos de machine learning.
-                * <strong>Classificação:</strong> Aqui você poderá fazer uma simulação para descobrir a previsão da situação e do estágio do paciente.
-                """, unsafe_allow_html=True)
-    st.markdown("<div class='container'><img src='https://raw.githubusercontent.com/ricktherunner/CirrosifierApp-TRAINEES1/main/img/linhas2t.gif' alt='GitHub'></div>", unsafe_allow_html=True)
+                    """, unsafe_allow_html=True)
+        
+        st.markdown("---")
+
+        # Navigation
+        st.header("Navegação")
+        st.markdown("Clique nos botões da barra lateral para navegar pelo site:")
+        st.markdown("""
+                    * <strong>Home:</strong> Onde você se encontra!
+                    * <strong>Projeto:</strong> Aqui você verá mais informações sobre o nosso projeto
+                    * <strong>Dados:</strong> Aqui você verá alguns gráficos gerados durante a análise do dataset e da aplicação dos modelos de machine learning.
+                    * <strong>Classificação:</strong> Aqui você poderá fazer uma simulação para descobrir a previsão da situação e do estágio do paciente.
+                    """, unsafe_allow_html=True)
+        st.markdown(f"<div class='container'><img src={self.linhas_gif_path} alt='GitHub'></div>", unsafe_allow_html=True)
